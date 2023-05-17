52417b506e58248828ffe8574b564c39c6f3c758
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 18:46:12 +0100

    Add chat support

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -19,6 +19,26 @@
     <!-- Dashboard CSS -->
     <link type="text/css" href="{{ asset('css/dash.css') }}" rel="stylesheet">
 
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'fc8eacf3d4954fb01620e09f6dee9e50d4d356c9';
+        window.smartsupp || (function(d) {
+            var s, c, o = smartsupp = function() {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </head>
 
 <body>
